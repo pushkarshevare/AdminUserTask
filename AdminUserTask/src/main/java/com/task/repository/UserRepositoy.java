2f550f2efb0166a2eb95ba77598adaff374f109b
Pushkar <95009665+pushkarshevare@users.noreply.github.com>
2024-01-31 20:05:00 +0530

    Delete AdminUserTask directory

--- AdminUserTask/src/main/java/com/task/repository/UserRepositoy.java
+++
@@ -1,9 +0,0 @@
-package com.task.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.task.model.User;
-
-public interface UserRepositoy extends JpaRepository<User, Integer>{
-
-}
